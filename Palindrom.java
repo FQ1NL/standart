b6368f2ef1e72afc27d9c3ae9f415bc0813ba499
FQ1NL <61064449+FQ1NL@users.noreply.github.com>
2020-02-21 00:01:49 +0100

    Add files via upload

---
+++ Palindrom.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrom {
+    public  static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String string = scan.nextLine();
+
+        scan.close();
+
+        int start = 0, end = string.length()-1;
+        boolean sonuc = true;
+        do {
+            if (string.charAt(start) != string.charAt(end)) sonuc = false;
+            start++;
+            end--;
+        } while (start <= end);
+
+        String res = sonuc ? "Palindrom!" : "Not Palindrom !";
+        System.out.println(res);
+    }
+}
